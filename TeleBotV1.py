43ec2b913033cc1b60d76786d5fffcd86c17e0b9
YongHao <fuyonghao94@gmail.com>
2020-01-31 12:41:36 +0800

    c1

--- TeleBotV1.py
+++ TeleBotV1.py
@@ -1,48 +1,79 @@
-from telegram.ext import Updater
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to reply to Telegram messages.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
 import logging
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler, Filters
-import os
-import random
 
-updater = Updater(token='1064584856:AAF9P6QEMj-F9o4585qeBR-7mY2-ciDCjLs', use_context=True)
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-dispatcher = updater.dispatcher
-
+# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-TYPING_REPLY = range(3)
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-def roast(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Enihow is short")
-
-def chiobu(update, context):
-    file = "/Users/geraldine/Desktop/untitled folder"
-    pic = random.choice(os.listdir(file))
-    print(pic)
-    pic = os.path.join(file, pic)
-    print(pic)
-    context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(pic,'rb'))
-
-def regular_choice(update, context):
-    text = update.message.text
-    context.user_data['choice'] = text
-    update.message.reply_text(
-        'Your {}? Yes, I would love to hear about that!'.format(text.lower()))
-
-    return TYPING_REPLY
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
 
-start_handler = CommandHandler('start', start)
-roast_handler = CommandHandler('chiobu', chiobu)
-regular_choice = CommandHandler('pick', regular_choice)
-chiobu_handler = CommandHandler('roast', roast)
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(roast_handler)
-dispatcher.add_handler(chiobu_handler)
-dispatcher.add_handler(regular_choice)
-updater.start_polling()
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text("No " + update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("1064584856:AAF9P6QEMj-F9o4585qeBR-7mY2-ciDCjLs", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
